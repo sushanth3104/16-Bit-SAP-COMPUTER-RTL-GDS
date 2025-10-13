2da83ed1422d752bee957fba6082c19ca03623fe
sushanth3104 <sushkumarthaviti@gmail.com>
2025-10-13 20:14:34 +0530

    ADD VERILOG SIM FILE STRUCTURE

--- Design/src/tb/run.sh
+++ Design/src/tb/run.sh
@@ -1,4 +1,4 @@
-iverilog -o sim_out.vvp tb.v sap.v|| exit 1
+iverilog -o sim_out.vvp tb.v include.v|| exit 1
 vvp sim_out.vvp || exit 1
 [ -s sim_output.vcd ] || exit 1
 surfer sim_output.vcd || exit 1
