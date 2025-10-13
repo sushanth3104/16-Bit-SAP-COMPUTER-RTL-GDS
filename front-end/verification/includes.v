365dddb74ab361e93665c1b87eb7d044ebf46e13
sushanth3104 <sushkumarthaviti@gmail.com>
2025-10-13 22:40:27 +0530

    CORRECTED PATHS

---
+++ front-end/verification/includes.v
@@ -0,0 +1,17 @@
+// Place defines first
+`include"../rtl/defines.v"
+
+// Module Codes
+`include"../rtl/accumulator.v"
+`include"../rtl/alu.v"
+`include"../rtl/b_register.v"
+`include"../rtl/cu.v"
+`include"../rtl/flag.v"
+`include"../rtl/ir.v"
+`include"../rtl/mar.v"
+`include"../rtl/out.v"
+`include"../rtl/pc.v"
+`include"../rtl/ram.v"
+
+// Top Module Codee
+`include"../rtl/sap.v"
