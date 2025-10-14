628a297142971a8e190c2689448285c188c9c8c2
sushanth3104 <sushkumarthaviti@gmail.com>
2025-10-14 11:55:59 +0530

    UPDATES NAMING

--- miscellaneous/manual-synthesis-sky130/include.v
+++ miscellaneous/manual-synthesis-sky130/include.v
@@ -1,17 +1,17 @@
 // Place defines first
-`include"../../front-end/rtl/defines.v"
+`include"../../RTL-SIM/rtl/defines.v"
 
 // Module Codes
-`include"../../front-end/rtl/accumulator.v"
-`include"../../front-end/rtl/alu.v"
-`include"../../front-end/rtl/b_register.v"
-`include"../../front-end/rtl/cu.v"
-`include"../../front-end/rtl/flag.v"
-`include"../../front-end/rtl/ir.v"
-`include"../../front-end/rtl/mar.v"
-`include"../../front-end/rtl/out.v"
-`include"../../front-end/rtl/pc.v"
-`include"../../front-end/rtl/ram.v"
+`include"../../RTL-SIM/rtl/accumulator.v"
+`include"../../RTL-SIM/rtl/alu.v"
+`include"../../RTL-SIM/rtl/b_register.v"
+`include"../../RTL-SIM/rtl/cu.v"
+`include"../../RTL-SIM/rtl/flag.v"
+`include"../../RTL-SIM/rtl/ir.v"
+`include"../../RTL-SIM/rtl/mar.v"
+`include"../../RTL-SIM/rtl/out.v"
+`include"../../RTL-SIM/rtl/pc.v"
+`include"../../RTL-SIM/rtl/ram.v"
 
 // Top Module Codee
-`include"../../front-end/rtl/sap.v"
+`include"../../RTL-SIM/rtl/sap.v"
